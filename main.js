3e5426d924e335e3bfacbd971febbd7d73223808
RulGanZz <155933386+Rul404@users.noreply.github.com>
2024-05-01 00:44:57 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+let awal = document.getElementById('awal')
+function myInfo() {
+    if(awal.style.display === 'none') {
+        awal.style.display = 'block'
+    } else {
+        awal.style.display ='none'
+    }
+}
+
+let pertama = document.getElementById('pertama')
+function myPertama() {
+    if(pertama.style.display === 'none') {
+        pertama.style.display = 'block'
+    } else {
+        pertama.style.display ='none'
+    }
+}
+
+let kedua = document.getElementById('kedua')
+function myKedua() {
+    if(kedua.style.display === 'none') {
+        kedua.style.display = 'block'
+    } else {
+        kedua.style.display ='none'
+    }
+}
+
+let ketiga = document.getElementById('ketiga')
+function myKetiga() {
+    if(ketiga.style.display === 'none') {
+        ketiga.style.display = 'block'
+    } else {
+        ketiga.style.display ='none'
+    }
+}
+
+let keempat = document.getElementById('keempat')
+function myKeempat() {
+    if(keempat.style.display === 'none') {
+        keempat.style.display = 'block'
+    } else {
+        keempat.style.display ='none'
+    }
+}
+
+let kelima = document.getElementById('kelima')
+function myKelima() {
+    if(kelima.style.display === 'none') {
+        kelima.style.display = 'block'
+    } else {
+        kelima.style.display ='none'
+    }
+}
